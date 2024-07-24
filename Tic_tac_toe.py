7264c36ccecaf1f2c3668f039cf460a0eff627bb
onanahauwa <onanahauwa@gmail.com>
2024-07-24 17:41:51 +0100

    Added two more functions

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -1,5 +1,25 @@
-def display(mylist):
-    print(mylist)
+def display(board):
+    print(board[7] + '|' + board[8] + '|' +  board[9])
+    print('-----')
+    print(board[4] + '|' + board[5] + '|' +  board[6])
+    print('-----')
+    print(board[1] + '|' + board[2] + '|' +  board[3])
 
-mylist = [0,1,2,3,4,5,6,7,8,9,10]
+
+mylist = [' '] * 10
+#display(mylist)
+
+def player_input():
+    choice = 'wrong'
+
+    while choice != 'X' and choice != 'O':
+        choice = input("Please choose between 'x' or 'o': ").upper()
+    
+    return choice
+#player_input()
+
+def place_marker(board, marker, position):
+    board[position] = marker
+    
+place_marker(mylist,'$',8)
 display(mylist)
